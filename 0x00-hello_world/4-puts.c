307b7e4a87e54e62f7f24acf6ed13fbba56d1c9b
Oluchiiii <onwaezeoluchukwu@gmail.com>
2022-04-07 08:54:01 -0700

    First C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 
 /**
- *
  *  * main - Entry point
  *
  *  *
  *
  *  * Return: always 0 (Success)
  *
- *
  */
-
-
 
 int main(void)
 
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
