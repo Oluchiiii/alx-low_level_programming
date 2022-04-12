e8395e1878f51f86687d4eac4db06f9178e64427
Oluchiiii <onwaezeoluchukwu@gmail.com>
2022-04-12 11:56:57 -0700

    Thrid commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - cmputes the absolute value of an integer
+ *
+ * @i: an input character
+ *
+ * Return: absolute value of i
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else 
+		return (i * -1);
+}
