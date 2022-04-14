1aaa1946edc4b833d3cc583d9787a7beeb0990e4
Oluchiiii <onwaezeoluchukwu@gmail.com>
2022-04-14 06:33:49 -0700

    Sixth commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point. Print Fizz, Buzz, and FizzBuzz
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int div;
+
+	for (div = 1; div <= 100; div++)
+	{
+		if ((div % 3 == 0) && (div % 5 == ))
+		{
+			printf("FizzBuzz")
+		}
+		else if (div % 3 == 0)
+		{
+			printf("Fizz")
+		}
+		else if (div % 5 == 0)
+		{
+			printf("Buzz")
+		}
+		else
+		{
+			printf("%d", div);
+		}
+		if (div < 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
