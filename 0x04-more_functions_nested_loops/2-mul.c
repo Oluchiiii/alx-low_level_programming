65ab8a25d7c7b60cd85dc60cfa0e98a0ff028ab6
Oluchiiii <onwaezeoluchukwu@gmail.com>
2022-04-14 04:45:56 -0700

    Second commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Entry point. Multiplies two integers
+ * @a: first integer value to be multiplied
+ * @b: second integer value to be multiplied
+ * Return: Returns the result of the multiplied a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
