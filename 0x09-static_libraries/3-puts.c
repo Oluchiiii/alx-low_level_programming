61a8d00e9ac8b30fdb2bebe15a1778b7c58abc5d
Onwaeze Oluchi <101384651+Oluchiiii@users.noreply.github.com>
2022-05-02 22:59:42 +0100

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,21 +1,18 @@
 #include "main.h"
-
 /**
- * _puts - prints a string, followed by a new line
- * 
- * @str: pointer to the string to be printed
- *
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
  * Return: void
  */
 
 void _puts(char *str)
 {
 	int i = 0;
-	
-	while (str[i])
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		i++;
 	}
+
 	_putchar('\n');
 }
