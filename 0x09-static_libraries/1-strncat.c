140e81316620de26371d27a67f6105e8811e77c1
Onwaeze Oluchi <101384651+Oluchiiii@users.noreply.github.com>
2022-05-02 22:53:35 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,31 +1,31 @@
 #include "main.h"
-
 /**
  * _strncat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ * @n: times to iterations
  *
- * @dest: destination
- * @src: source
- * @n: number of bytes used from src
- *
- * Return: pointer to dest
+ * Return: dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int count = 0, count2 = 0;
+	int i;
+	int j;
 
-	while (*(dest + count) != '\0')
+	i = 0;
+
+	while (dest[i] != 0)
 	{
-		count++;
+		i++;
 	}
 
-	while (count2 < n)
+	j = 0;
+
+	while (src[j] != 0 && j < n)
 	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	return (dest);
 }
