8bacc4221a47b16fc073d554abd6b39b9e70d499
Onwaeze Oluchi <101384651+Oluchiiii@users.noreply.github.com>
2022-05-02 23:05:17 +0100

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,34 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strstr - finds the first occurence of the substring
- * needle in the string haystack
+ * _strstr - locates a substring
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack
  *
- * @haystack: entire string
- * @needle: substring
- *
- * Return: pointer to the beginning of located substring or
- * NULL if the substring is not found
+ * Return: pointer to beginning of needle in haystack or NULL if no match
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	unsigned int i = 0, j = 0;
 
-	for (i = 0; haystack[i] > '\0'; i++)
+	while (haystack[i])
 	{
-		for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			if (haystack[j] != needle[j - i])
-			{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
 				break;
-			}
 		}
-		if (needle[j - i] == '\0')
+		if (needle[j])
 		{
-			return (haystack + 1);
+			i++;
+			j = 0;
 		}
+		else
+			return (haystack + i);
 	}
 	return (0);
 }
