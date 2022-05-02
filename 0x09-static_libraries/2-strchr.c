54b1f35f6da1b584bb03179b2780d033a3ad8b36
Onwaeze Oluchi <101384651+Oluchiiii@users.noreply.github.com>
2022-05-02 22:55:56 +0100

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * *_strchr - locates a character in a string
+ * _strchr - prints buffer in hexa
+ * @s: buffer
+ * @c: buffer2
  *
- * @s: string
- * @c: character
- *
- * Return: pointer to first occurece of the character c
+ * Return: Nothing.
  */
-
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
-
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s+ i);
-	if (*(s + i) == c)
-		return (s + i);
-	return ('\0');
+	while (*s != c)
+	{
+		if (!*s++)
+		{
+			return (0);
+		}
+	}
+		return (s);
 }
