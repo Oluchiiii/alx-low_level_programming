2b1e1b2e887a389a854f0b3623a096bab6b7156a
Oluchiiii <onwaezeoluchukwu@gmail.com>
2022-05-13 20:30:00 -0700

    Second commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,12 @@
 
 /**
  * print_name - prints a name as is
+ *
  * @name: name of the person
  * @f: is pointer
+ *
  * Return: Nothing.
-**/
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
