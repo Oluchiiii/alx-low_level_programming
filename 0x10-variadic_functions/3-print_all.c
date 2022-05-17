8ab6715c4401fecd9654d63d4cac17750fb540c0
Onwaeze Oluchi <101384651+Oluchiiii@users.noreply.github.com>
2022-05-17 01:46:18 +0100

    Variadic funcions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,12 +2,9 @@
 
 /**
  * print_all - prints anything
- *
- * @format: list of type of arguments passed to the function
- *
+ * @format: list of types of arguments passed to the function
  */
-
-void print_all(const char *i const format, ...)
+void print_all(const char * const format, ...)
 {
 	int i = 0;
 	char *str, *sep = "";
@@ -29,7 +26,7 @@ void print_all(const char *i const format, ...)
 					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
-					printf("%s%f", sep, va_arg(list, int));
+					printf("%s%f", sep, va_arg(list, double));
 					break;
 				case 's':
 					str = va_arg(list, char *);
